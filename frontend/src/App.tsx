5ba672f98ec5d948e552f37e642be336980828ce
tuvsheezz <tuvshee0089@gmail.com>
2023-10-10 15:30:08 +0900

    k8s

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   const [text, setText] = useState<string | null>(null);
 
   useEffect(() => {
-    axios.get("/api/hello").then((response) => setText(response.data.data));
+    axios.get("10.96.0.1:8000/api/hello").then((response) => setText(response.data.data));
   }, []);
 
   return (
